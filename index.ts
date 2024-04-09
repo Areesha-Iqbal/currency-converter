6038378b5417a63283df4aeb89dd825e5abb1ee6
Areesha-Iqbal <areeshaiqbal319@gmail.com>
2024-04-09 04:44:49 -0700

    currency converter project

---
+++ index.ts
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.cyan.bold("\t>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"));
+console.log(chalk.green.bold("\tWelcome to 'Areesha Iqbal' Currency Converter"));
+console.log(chalk.cyan.bold("\t>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"));
+
+
+const currency: any = {
+  USD: 1, // Base currency
+  EUR: 0.91,
+  GBP: 0.76,
+  INR: 74.57,
+  PKR: 280,
+};
+
+let user_answer = await inquirer.prompt([
+  {
+    name: "from",
+    message: chalk.yellow.italic("Enter from Currency"),
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "to",
+    message: chalk.yellow.italic("Enter To Currency"),
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "amount",
+    message: chalk.yellow.italic("Enter Your Amount"),
+    type: "number",
+  },
+]);
+
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(Math.round(convertedAmount));
